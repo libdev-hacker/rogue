01cf2324ef623b5897a3153392392f4061d13941
libdev-hacker <libdev-hacker@tuta.io>
2026-01-24 20:05:53 +0000

    Added extension method for HTMLElement
    - Populates all the attributes from XmlReader

---
+++ src/Rogue.HTML/HTMLElementExtensions.cs
@@ -0,0 +1,21 @@
+using System.Diagnostics;
+using System.Xml;
+
+namespace Rogue.HTML
+{
+    public static class HTMLElementExtensions
+    {
+        public static void PopulateAttributes(this HTMLElement element, XmlTextReader reader)
+        {
+            if (reader.HasAttributes)
+            {
+                for (int i = 0; i < reader.AttributeCount; i++)
+                {
+                    reader.MoveToAttribute(i);
+                    element.AddAttribute(reader.Name, reader.Value);
+                }
+                reader.MoveToElement();
+            }
+        }
+    }
+}
