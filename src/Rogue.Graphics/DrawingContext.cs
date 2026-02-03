864125ac27cc78ca81ba8c433b2a9dba38c10e55
libdev-hacker <libdev-hacker@tuta.io>
2026-02-03 14:43:00 +0000

    Changed DrawingContext greatly
    - Changed most fields into read-only properties
    - Added VAO support
    - Reworked validation throughout

--- src/Rogue.Graphics/DrawingContext.cs
+++ src/Rogue.Graphics/DrawingContext.cs
@@ -4,13 +4,19 @@ namespace Rogue.Graphics
 {
     public class DrawingContext: IDisposable
     {
-        private int _shader;
+        public int Shader { get; private set; }
+
+        public IDictionary<string, int>? Textures { get => field?.AsReadOnly(); }
+        
+        public int Vbo { get; private set; }
+        
+        public int? Ebo { get; private set; }
+
+        public bool IsBuffersSet { get => !(this.Vbo == default || this.Ebo == default); }
+
+        public readonly int Vao = GL.GenVertexArray();
 
         private Dictionary<string, int>? _textures;
-
-        private int _vbo;        
-        
-        private int? _ebo;
 
         private bool _disposed;
 
@@ -29,7 +35,7 @@ namespace Rogue.Graphics
         {
             if (GL.IsShader(handle))
             {
-                _shader = handle;
+                this.Shader = handle;
             }
         }
 
@@ -37,7 +43,7 @@ namespace Rogue.Graphics
         {
             if (GL.IsBuffer(buffer.Handle) && buffer.BufferType == BufferTarget.ArrayBuffer)
             {
-                _vbo = buffer.Handle;
+                this.Vbo = buffer.Handle;
             }
         }
 
@@ -45,15 +51,21 @@ namespace Rogue.Graphics
         {
             if (GL.IsBuffer(buffer.Handle) && buffer.BufferType == BufferTarget.ElementArrayBuffer)
             {
-                _ebo ??= buffer.Handle;
+                this.Ebo ??= buffer.Handle;
             }
+        }
+
+        public void AddAttributePointer(int index, int count, VertexAttribPointerType type, int stride, int offset = 0)
+        {
+            GL.VertexAttribPointer(index, count, type, false, stride, offset);
+            GL.EnableVertexAttribArray(index);
         }
 
         public void UseShader()
         {
-            if (GL.IsShader(_shader))
+            if (GL.IsShader(this.Shader))
             {
-                GL.UseProgram(_shader);
+                GL.UseProgram(this.Shader);
             }
         }
 
@@ -69,10 +81,18 @@ namespace Rogue.Graphics
         {
             if (target == BufferTarget.ArrayBuffer)
             {
-                GL.BindBuffer(target, _vbo);
+                GL.BindBuffer(target, this.Vbo);
             } else if (target == BufferTarget.ElementArrayBuffer)
             {
-                if (_ebo is not null) GL.BindBuffer(target, (int)_ebo); // More annoying
+                if (this.Ebo is not null) GL.BindBuffer(target, (int)this.Ebo); // More annoying
+            }
+        }
+
+        public void BindVao()
+        {
+            if (GL.IsVertexArray(this.Vao))
+            {
+                GL.BindVertexArray(this.Vao);
             }
         }
 
@@ -101,19 +121,19 @@ namespace Rogue.Graphics
                     _textures = null;
 
                     GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
-                    GL.DeleteBuffer(_vbo);
+                    GL.DeleteBuffer(this.Vbo);
 
-                    _vbo = 0;
+                    this.Vbo = 0;
 
-                    if (_ebo is not null)
+                    if (this.Ebo is not null)
                     {
                         GL.BindBuffer(BufferTarget.ElementArrayBuffer, 0);
-                        GL.DeleteBuffer((int)_ebo);
-                        _ebo = 0;
+                        GL.DeleteBuffer((int)this.Ebo);
+                        this.Ebo = 0;
                     }
 
-                    GL.DeleteProgram(_shader);
-                    _shader = 0;
+                    GL.DeleteProgram(this.Shader);
+                    this.Shader = 0;
 
                     _disposed = true;
                 }
