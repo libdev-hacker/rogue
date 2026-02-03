422fe84e310aff58d3b5723baba7eab86498ab62
libdev-hacker <libdev-hacker@tuta.io>
2026-02-03 14:43:38 +0000

    Implemented Draw method in HTML text elements
    - First attempt

--- src/Rogue.HTML/HTMLTextElement.cs
+++ src/Rogue.HTML/HTMLTextElement.cs
@@ -1,5 +1,10 @@
 using System.Text;
 
+using OpenTK.Graphics.OpenGL4;
+using Rogue.Graphics;
+using Rogue.Utils;
+using GraphicsBuffer = Rogue.Graphics.Buffer;
+
 namespace Rogue.HTML
 {
     public class HTMLTextElement: HTMLElement
@@ -11,5 +16,51 @@ namespace Rogue.HTML
         public static readonly string[] SupportedTags = [ "p", "div" ];
 
         public override void AddText(string text) => _text.Append(text);
+
+        public override void Draw()
+        {
+            float[] vertices = this.Container.GetCoords(this.Depth);
+
+            string id = Convert.ToString(this.GetHashCode());
+
+            bool hasTexture = false;
+
+            if (this.Renderer.Textures is not null)
+            {
+                if (!this.Renderer.Textures.ContainsKey(id))
+                {
+                    int renderedText = TextRenderer.CreateText(this, ref vertices);
+                    this.Renderer.AddTexture($"{this.GetHashCode()}", renderedText);
+                    hasTexture = true;
+                } else
+                {
+                    hasTexture = true;
+                }
+            }
+
+
+            if (!this.Renderer.IsBuffersSet)
+            {
+                GraphicsBuffer vbo = new (vertices, BufferTarget.ArrayBuffer, BufferUsageHint.DynamicDraw);
+                GraphicsBuffer ebo = new (BufferUsageHint.StaticDraw);
+                
+                this.Renderer.AddVertexBufferObject(vbo);
+                this.Renderer.AddElementBufferObject(ebo);
+            }
+
+
+            this.Renderer.BindBuffer(BufferTarget.ArrayBuffer);
+            this.Renderer.BindVao();
+            this.Renderer.BindBuffer(BufferTarget.ElementArrayBuffer);
+
+            this.Renderer.AddAttributePointer(0, 3, VertexAttribPointerType.Float, 5 * sizeof(float));
+            if (hasTexture)
+            {
+                int textureLocation = Shader.GetAttributeLocation(this.Renderer.Shader, "texCoords");
+                this.Renderer.AddAttributePointer(textureLocation, 2, VertexAttribPointerType.Float, 5 * sizeof(float), 3 * sizeof(float));
+            }
+
+            GL.DrawElements(BeginMode.Triangles, 6, DrawElementsType.UnsignedInt, 0);
+        }
     }
 }
