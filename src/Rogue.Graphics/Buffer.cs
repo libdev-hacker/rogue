4caa0ada8307a3ea5fc6b785f75b71b10241c67e
libdev-hacker <libdev-hacker@tuta.io>
2026-02-08 16:37:03 +0000

    Slimmed down Buffer struct
    - Moved more relevant data into DrawingContext

--- src/Rogue.Graphics/Buffer.cs
+++ src/Rogue.Graphics/Buffer.cs
@@ -9,24 +9,14 @@ namespace Rogue.Graphics
         public BufferTarget BufferType { get; }
 
         public static uint[] Indices = [
-            0, 1, 2,
+            0, 1, 3,
             1, 2, 3
         ]; // From LearnOpenGL
 
-        public Buffer(float[] data, BufferTarget kind, BufferUsageHint hint)
+        public Buffer(BufferTarget kind)
         {
             this.Handle = GL.GenBuffer();
             this.BufferType = kind;
-
-            GL.BufferData(kind, data.Length * sizeof(float), data, hint);
-        }
-
-        public Buffer(BufferUsageHint hint, BufferTarget kind = BufferTarget.ElementArrayBuffer)
-        {
-            this.Handle = GL.GenBuffer();
-            this.BufferType = kind;
-
-            GL.BufferData(kind, Buffer.Indices.Length * sizeof(uint), Buffer.Indices, hint);
         }
     }
 }
