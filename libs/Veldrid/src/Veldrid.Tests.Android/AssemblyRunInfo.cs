63e6794ae2144bf8e292e8142970138c99a2a271
libdev-hacker <libdev-hacker@tuta.io>
2026-07-06 15:25:08 +0100

    refactor(Veldrid): Removed unnecessary projects

--- libs/Veldrid/src/Veldrid.Tests.Android/AssemblyRunInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using System.Reflection;
-using Veldrid.Tests.Android.ViewModels;
-using Xunit;
-
-namespace Veldrid.Tests.Android
-{
-    public record AssemblyRunInfo(Assembly Assembly, TestAssemblyConfiguration Configuration, List<TestCaseViewModel> TestCases);
-}
