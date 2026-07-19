5540714fd23fa99fc4ef83070e3169ec3fa1702b
libdev-hacker <libdev-hacker@tuta.io>
2026-07-19 18:47:13 +0100

    refactor(Graphics): Moved PipelineDescription instantiation into OpenGLResources

--- src/Rogue.Graphics/Backends/OpenGLResources.cs
+++ src/Rogue.Graphics/Backends/OpenGLResources.cs
@@ -6,5 +6,22 @@ namespace Rogue.Graphics.Backends
     public static class OpenGLResources
     {
         public static GraphicsDevice? Device { get; set; }
+
+        internal static GraphicsPipelineDescription CreatePipeline() => new ()
+        {
+            BlendState = BlendStateDescription.SingleOverrideBlend,
+            DepthStencilState = new(
+                true,
+                true,
+                ComparisonKind.Greater // CSS z-index has greatest infront
+            ),
+            RasterizerState = new (
+                FaceCullMode.Back,
+                PolygonFillMode.Solid,
+                FrontFace.CounterClockwise, // OpenGL uses Counter-Clockwise by default
+                false,
+                false
+            )
+        };
     }
 }
